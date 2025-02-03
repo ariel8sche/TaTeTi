bd23bb622032ad469d3041c3093b4d7905854464
ariel8sche <aschenone@dc.uba.ar>
2025-02-03 19:24:45 -0300

    Juego terminado

--- Tateti/Program.cs
+++ Tateti/Program.cs
@@ -13,13 +13,62 @@ namespace Tateti
 
         static void Main(string[] args)
         {
+            bool terminado = false;
+
             //Funcion encargada de crear el tablero
-            CrearTablero();
+            DibujarTablero();
             Console.WriteLine("\nJugador 1: O\nJugador 2: X");
+
+            do
+            {
+                //Turno del jugador 1
+                PreguntarPosicion(1);
+
+                //Dibujar eleccion del jugador 1
+                DibujarTablero();
+
+                terminado = VerificarGanador();
+
+                if (terminado)
+                {
+                    Console.WriteLine("\n¡Jugador 1 ha ganado!");
+                }
+                else
+                {
+                    terminado = VerificarEmpate();
+                    if (terminado)
+                    {
+                        Console.WriteLine("\n¡Empate!");
+                    }
+                    else
+                    {
+                        //Turno del jugador 2
+                        PreguntarPosicion(2);
+
+                        //Dibujar eleccion del jugador 2
+                        DibujarTablero();
+
+                        terminado = VerificarGanador();
+
+                        if (terminado)
+                        {
+                            Console.WriteLine("\n¡Jugador 2 ha ganado!");
+                        }
+                        else
+                        {
+                            terminado = VerificarEmpate();
+                            if (terminado)
+                            {
+                                Console.WriteLine("\n¡Empate!");
+                            }
+                        }
+                    }
+                }
+            } while (terminado == false);
 
         }// Fin de la función Main
 
-        static void CrearTablero()
+        static void DibujarTablero()
         {
             // Variables del ciclo anidado
             int fila=0, columna = 0;
@@ -73,6 +122,57 @@ namespace Tateti
             tablero[fila - 1, columna - 1] = jugador;
         }// Fin de la función PreguntarPosicion
 
+        static bool VerificarGanador()
+        {
+            int fila=0, columna = 0;
+            bool ticTacToe = false;
+
+            for (fila = 0; fila < 3; fila++)
+            {
+                if (tablero[fila, 0] == tablero[fila, 1] && tablero[fila, 1] == tablero[fila, 2] && tablero[fila, 0] != 0)
+                {
+                    ticTacToe = true;
+                }
+            }
+
+            for (columna = 0; columna < 3; columna++)
+            {
+                if (tablero[0, columna] == tablero[1, columna] && tablero[1, columna] == tablero[2, columna] && tablero[0, columna] != 0)
+                {
+                    ticTacToe = true;
+                }
+            }
+
+            if (tablero[0, 0] == tablero[1, 1] && tablero[1, 1] == tablero[2, 2] && tablero[0, 0] != 0)
+            {
+                ticTacToe = true;
+            }
+
+            if (tablero[0, 2] == tablero[1, 1] && tablero[1, 1] == tablero[2, 0] && tablero[0, 2] != 0)
+            {
+                ticTacToe = true;
+            }
+
+            return ticTacToe;
+        }// Fin de la función VerificarGanador
+
+        static bool VerificarEmpate()
+        {
+            bool hayEspacio = false;
+            int fila, columna;
+
+            for (fila = 0; fila < 3; fila++)
+            {
+                for (columna = 0; columna < 3; columna++)
+                {
+                    if (tablero[fila, columna] == 0)
+                    {
+                        hayEspacio = true;
+                    }
+                }
+            }
+            return !hayEspacio;
+        }// Fin de la función VerificarEmpate
 
     }
 }
