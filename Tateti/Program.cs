103c706b03e71944d5413cc33f8d415407a7e2a1
ariel8sche <aschenone@dc.uba.ar>
2025-02-03 18:10:59 -0300

    Creando tablero

--- Tateti/Program.cs
+++ Tateti/Program.cs
@@ -13,6 +13,31 @@ namespace Tateti
 
         static void Main(string[] args)
         {
-        }
+            //Funcion encargada de crear el tablero
+            CrearTablero();
+            Console.WriteLine("\nJugador 1: O\nJugador 2: X");
+
+        }// Fin de la función Main
+
+        static void CrearTablero()
+        {
+            // Variables del ciclo anidado
+            int fila=0, columna = 0;
+
+            Console.WriteLine(); // Espacio en blanco
+            string guiones = new string('-', 13);
+            Console.WriteLine(guiones);
+
+            for (fila=0; fila < 3; fila++)
+            {
+                Console.Write("|");
+                for (columna = 0; columna < 3; columna++)
+                {
+                    Console.Write(" " + simbolos[tablero[fila, columna]] + " |");
+                }
+                Console.WriteLine($"\n{guiones}");
+            }
+
+        }// Fin de la función CrearTablero
     }
 }
